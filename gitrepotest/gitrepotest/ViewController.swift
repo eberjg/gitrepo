99aa42fec3249c7e4d54fb76ea9b6f5f502de113
Eber <eberjg@gmail.com>
2018-05-10 22:18:16 -0400

    first commit now

---
+++ gitrepotest/gitrepotest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  gitrepotest
+//
+//  Created by eber on 5/10/18.
+//  Copyright © 2018 eber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("el primer lugar")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
